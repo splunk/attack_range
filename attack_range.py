3484d1e0fae83968b2a22d8c263f968b27854db1
Patrick Bareiss <pbareib@splunk.com>
2019-11-18 14:00:42 +0100

    build should work for terraform and vagrant

--- attack_range.py
+++ attack_range.py
@@ -11,51 +11,51 @@ os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
 
 VERSION = 1
 
-def grab_splunk(bin_dir):
-    print("\ngrabbing splunk enterprise server for linux\n")
-    url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.3.1&product=splunk&filename=splunk-7.3.1-bd63e13aa157-Linux-x86_64.tgz&wget=true'
-    output = bin_dir + '/splunk-7.3.1-bd63e13aa157-Linux-x86_64.tgz'
-    wget.download(url,output)
-
-def grab_splunk_uf_win(bin_dir):
-    print("\ngrabbing splunk forwarder for windows\n")
-    url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=7.3.0&product=universalforwarder&filename=splunkforwarder-7.3.0-657388c7a488-x64-release.msi&wget=true'
-    output = bin_dir + '/splunkforwarder-7.3.0-657388c7a488-x64-release.msi'
-    wget.download(url,output)
-
-def grab_splunk_ta_win(bin_dir):
-    print("\ngrabbing splunk add-on for windows\n")
-    url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/splunk-add-on-for-microsoft-windows_600.tgz'
-    output = bin_dir + '/splunk-add-on-for-microsoft-windows_600.tgz'
-    wget.download(url, output)
-
-def grab_splunk_ta_sysmon(bin_dir):
-    print("\ngrabbing splunk add-on for sysmon\n")
-    url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/add-on-for-microsoft-sysmon_800.tgz'
-    output = bin_dir + '/add-on-for-microsoft-sysmon_800.tgz'
-    wget.download(url, output)
-
-def grab_splunk_cim_app(bin_dir):
-    print("\ngrabbing splunk (CIM) common information model app\n")
-    url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/splunk-common-information-model-cim_4130.tgz'
-    output = bin_dir + '/splunk-common-information-model-cim_4130.tgz'
-    wget.download(url, output)
-
-def grab_streams(bin_dir):
-    print("\ngrabbing splunk stream app\n")
-    url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/splunk-stream_713.tgz'
-    output = bin_dir + '/splunk-stream_713.tgz'
-    wget.download(url,output)
-    print("\ngrabbing splunk stream TA\n")
-    url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/Splunk_TA_stream.zip'
-    output = bin_dir + '/Splunk_TA_stream.zip'
-    wget.download(url,output)
-
-def grab_escu_latest(bin_dir):
-    print("\ngrabbing splunk ESCU app\n")
-    url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/DA-ESS-ContentUpdate-v1.0.41.tar.gz'
-    output = bin_dir + '/DA-ESS-ContentUpdate-v1.0.41.tar.gz'
-    wget.download(url,output)
+# def grab_splunk(bin_dir):
+#     print("\ngrabbing splunk enterprise server for linux\n")
+#     url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.3.1&product=splunk&filename=splunk-7.3.1-bd63e13aa157-Linux-x86_64.tgz&wget=true'
+#     output = bin_dir + '/splunk-7.3.1-bd63e13aa157-Linux-x86_64.tgz'
+#     wget.download(url,output)
+#
+# def grab_splunk_uf_win(bin_dir):
+#     print("\ngrabbing splunk forwarder for windows\n")
+#     url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=7.3.0&product=universalforwarder&filename=splunkforwarder-7.3.0-657388c7a488-x64-release.msi&wget=true'
+#     output = bin_dir + '/splunkforwarder-7.3.0-657388c7a488-x64-release.msi'
+#     wget.download(url,output)
+#
+# def grab_splunk_ta_win(bin_dir):
+#     print("\ngrabbing splunk add-on for windows\n")
+#     url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/splunk-add-on-for-microsoft-windows_600.tgz'
+#     output = bin_dir + '/splunk-add-on-for-microsoft-windows_600.tgz'
+#     wget.download(url, output)
+#
+# def grab_splunk_ta_sysmon(bin_dir):
+#     print("\ngrabbing splunk add-on for sysmon\n")
+#     url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/add-on-for-microsoft-sysmon_800.tgz'
+#     output = bin_dir + '/add-on-for-microsoft-sysmon_800.tgz'
+#     wget.download(url, output)
+#
+# def grab_splunk_cim_app(bin_dir):
+#     print("\ngrabbing splunk (CIM) common information model app\n")
+#     url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/splunk-common-information-model-cim_4130.tgz'
+#     output = bin_dir + '/splunk-common-information-model-cim_4130.tgz'
+#     wget.download(url, output)
+#
+# def grab_streams(bin_dir):
+#     print("\ngrabbing splunk stream app\n")
+#     url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/splunk-stream_713.tgz'
+#     output = bin_dir + '/splunk-stream_713.tgz'
+#     wget.download(url,output)
+#     print("\ngrabbing splunk stream TA\n")
+#     url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/Splunk_TA_stream.zip'
+#     output = bin_dir + '/Splunk_TA_stream.zip'
+#     wget.download(url,output)
+#
+# def grab_escu_latest(bin_dir):
+#     print("\ngrabbing splunk ESCU app\n")
+#     url = 'https://attack-range-appbinaries.s3-us-west-2.amazonaws.com/DA-ESS-ContentUpdate-v1.0.41.tar.gz'
+#     output = bin_dir + '/DA-ESS-ContentUpdate-v1.0.41.tar.gz'
+#     wget.download(url,output)
 
 
 def config_simulation(simulation_engine, simulation_technique):
@@ -223,7 +223,7 @@ if __name__ == "__main__":
                         help="please select a simulation engine, defaults to \"atomic_red_team\"")
     parser.add_argument("-st", "--simulation_technique", required=False, type=str, default="",
                         help="comma delimited list of MITRE ATT&CK technique ID to simulate in the attack_range, example: T1117, T1118, requires --simulation flag")
-    parser.add_argument("-b", "--appbin", required=False, default="appbinaries", help="directory to store binaries in")
+    #parser.add_argument("-b", "--appbin", required=False, default="appbinaries", help="directory to store binaries in")
     parser.add_argument("-v", "--version", required=False, help="shows current attack_range version")
 
     # parse them
@@ -262,21 +262,21 @@ starting program loaded for mode - B1 battle droid
         print("version: {0}".format(VERSION))
         sys.exit(1)
 
-    if os.path.exists(bin_dir):
-        print("this is not our first run binary directory exists, skipping setup")
-    else:
-        print("seems this is our first run, creating a directory for binaries at {0}".format(bin_dir))
-        os.makedirs(bin_dir)
-        grab_splunk(bin_dir)
-        grab_splunk_uf_win(bin_dir)
-        grab_splunk_ta_win(bin_dir)
-        grab_splunk_ta_sysmon(bin_dir)
-        grab_splunk_cim_app(bin_dir)
-        grab_streams(bin_dir)
-        grab_escu_latest(bin_dir)
+    # if os.path.exists(bin_dir):
+    #     print("this is not our first run binary directory exists, skipping setup")
+    # else:
+    #     print("seems this is our first run, creating a directory for binaries at {0}".format(bin_dir))
+    #     os.makedirs(bin_dir)
+    #     grab_splunk(bin_dir)
+    #     grab_splunk_uf_win(bin_dir)
+    #     grab_splunk_ta_win(bin_dir)
+    #     grab_splunk_ta_sysmon(bin_dir)
+    #     grab_splunk_cim_app(bin_dir)
+    #     grab_streams(bin_dir)
+    #     grab_escu_latest(bin_dir)
 
-    if vagrant_list:
-        list_vagrant_boxes()
+    # if vagrant_list:
+    #     list_vagrant_boxes()
 
     if action != "simulate" and simulation_technique[0] != "":
         print("you must specify action simulate if using a simulation technique")
