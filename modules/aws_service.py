12776e32a2f23b9623848e94781039e9586d2685
Patrick Bareiss <pbareib@splunk.com>
2019-12-10 10:29:25 +0100

    WIP search command

---
+++ modules/aws_service.py
@@ -0,0 +1,82 @@
+import sys
+import re
+import boto3
+from python_terraform import *
+
+
+
+def get_instance_by_name(ec2_name, log):
+    instances = get_all_instances()
+    for instance in instances:
+        str = instance['Tags'][0]['Value']
+        if str == ec2_name:
+            return instance
+
+
+def get_all_instances():
+    key_name = get_key_name()
+    client = boto3.client('ec2')
+    response = client.describe_instances(
+        Filters=[
+            {
+                'Name': "key-name",
+                'Values': [key_name]
+            }
+        ]
+    )
+    instances = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            str = instance['Tags'][0]['Value']
+            if str.startswith('attack-range'):
+                instances.append(instance)
+
+    return instances
+
+#instance['NetworkInterfaces'][0]['Association']['PublicIp']
+
+
+def check_ec2_instance_state(ec2_name, state):
+    instance = get_instance_by_name(ec2_name)
+
+    if not instance:
+        log.error(ec2_name + ' not found as AWS EC2 instance.')
+        sys.exit(1)
+
+    return (instance['State']['Name'] == state)
+
+
+def change_ec2_state(instances, new_state, log):
+
+    client = boto3.client('ec2')
+
+    if len(instances) == 0:
+        log.error(ec2_name + ' not found as AWS EC2 instance.')
+        sys.exit(1)
+
+    if new_state == 'stopped':
+        for instance in instances:
+            if instance['State']['Name'] == 'running':
+                response = client.stop_instances(
+                    InstanceIds=[instance['InstanceId']]
+                )
+                log.info('Successfully stopped instance with ID ' +
+                      instance['InstanceId'] + ' .')
+
+    elif new_state == 'running':
+        for instance in instances:
+            if instance['State']['Name'] == 'stopped':
+                response = client.start_instances(
+                    InstanceIds=[instance['InstanceId']]
+                )
+                log.info('Successfully started instance with ID ' + instance['InstanceId'] + ' .')
+
+
+def get_key_name():
+    with open('terraform/terraform.tfvars', 'r') as file:
+        terraformvars = file.read()
+
+    pattern = 'key_name = \"([^\"]*)'
+    a = re.search(pattern, terraformvars)
+
+    return a.group(1)
---
+++ modules/splunk_sdk.py
@@ -0,0 +1,60 @@
+import sys
+from time import sleep
+import splunklib.results as results
+import splunklib.client as client
+import splunklib.results as results
+
+
+def search(splunk_host, splunk_password, search_name, log):
+
+    service = client.connect(
+        host=splunk_host,
+        port=8089,
+        username='admin',
+        password=splunk_password
+    )
+
+    # Retrieve the new search
+    mysavedsearch = service.saved_searches[search_name]
+
+    # Run the saved search
+    job = mysavedsearch.dispatch()
+
+    # Create a small delay to allow time for the update between server and client
+    sleep(2)
+
+    # Wait for the job to finish--poll for completion and display stats
+    while True:
+        job.refresh()
+        stats = {"isDone": job["isDone"],
+                 "doneProgress": float(job["doneProgress"])*100,
+                  "scanCount": int(job["scanCount"]),
+                  "eventCount": int(job["eventCount"]),
+                  "resultCount": int(job["resultCount"])}
+        status = ("\r%(doneProgress)03.1f%%   %(scanCount)d scanned   "
+                  "%(eventCount)d matched   %(resultCount)d results") % stats
+
+        sys.stdout.write(status)
+        sys.stdout.flush()
+        if stats["isDone"] == "1":
+            break
+        sleep(2)
+
+    # Get the results and display them
+    for result in results.ResultsReader(job.results()):
+        print()
+        print(result)
+
+
+
+def list_searches(splunk_host, splunk_password):
+
+    service = client.connect(
+        host=splunk_host,
+        port=8089,
+        username='admin',
+        password=splunk_password
+    )
+
+    # List the saved searches that are available to the current user
+    return service.saved_searches
