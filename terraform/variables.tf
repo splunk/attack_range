ae0521fbff9ad8bc23631ef5e91fb83f1beb6866
jzsplunk <jzadeh@splunk.com>
2020-03-09 07:35:03 -0700

    [AR-239] testing deployment with streams app enabled. fix packer related variables./

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -87,6 +87,7 @@ variable "splunk_cim_app" { }
 variable "splunk_sysmon_ta" { }
 variable "splunk_python_app" { }
 variable "splunk_mltk_app" { }
+variable "splunk_stream_app" { }
 variable "install_es" { }
 variable "splunk_es_app" { }
 
