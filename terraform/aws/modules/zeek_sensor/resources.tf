f946a81cc39cd66045cb84e5aee3d1b719f39da6
davisshannon <williamshannondavis@gmail.com>
2020-12-01 12:22:14 +1100

    Added support for Windows Client
    Traffic mirror additions for Windows Client along with provisioner.

--- terraform/aws/modules/zeek_sensor/resources.tf
+++ terraform/aws/modules/zeek_sensor/resources.tf
@@ -39,14 +39,14 @@ resource "aws_instance" "zeek_sensor" {
 
   provisioner "local-exec" {
     working_dir = "../../ansible/"
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.config.private_key_path} -i '${aws_instance.zeek_sensor[0].public_ip},' playbooks/zeek.yml -e 'ansible_python_interpreter=/usr/bin/python3 splunk_uf_url=${var.config.splunk_uf_linux_deb_url} splunk_uf_binary=${var.config.splunk_uf_binary} windows_domain_controller_zeek_capture=${var.config.windows_domain_controller_zeek_capture} windows_server_zeek_capture=${var.config.windows_server_zeek_capture} splunk_indexer_ip=${var.config.splunk_server_private_ip}'"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.config.private_key_path} -i '${aws_instance.zeek_sensor[0].public_ip},' playbooks/zeek.yml -e 'ansible_python_interpreter=/usr/bin/python3 splunk_uf_url=${var.config.splunk_uf_linux_deb_url} splunk_uf_binary=${var.config.splunk_uf_binary} windows_domain_controller_zeek_capture=${var.config.windows_domain_controller_zeek_capture} windows_server_zeek_capture=${var.config.windows_server_zeek_capture} windows_client_zeek_capture=${var.config.windows_client_zeek_capture} splunk_indexer_ip=${var.config.splunk_server_private_ip}'"
   }
 }
 
-resource "aws_eip" "zeek_ip" {
-  count = var.config.zeek_sensor == "1" ? 1 : 0
-  instance      = aws_instance.zeek_sensor[0].id
-}
+#resource "aws_eip" "zeek_ip" {
+#  count = var.config.zeek_sensor == "1" ? 1 : 0
+#  instance      = aws_instance.zeek_sensor[0].id
+#}
 
 resource "aws_ec2_traffic_mirror_target" "zeek_target" {
   count = var.config.zeek_sensor == "1" ? 1 : 0
@@ -99,4 +99,14 @@ resource "aws_ec2_traffic_mirror_session" "zeek_windows_server_session" {
   traffic_mirror_target_id = aws_ec2_traffic_mirror_target.zeek_target[0].id
   network_interface_id     = var.windows_server_instance[0].primary_network_interface_id
   session_number           = 200
+}
+
+resource "aws_ec2_traffic_mirror_session" "zeek_windows_client_session" {
+  count         = var.config.windows_client_zeek_capture == "1" && var.config.zeek_sensor == "1" ? 1 : 0
+  description              = "Zeek Mirror Session for Windows Client"
+  depends_on = [var.windows_client_instance]
+  traffic_mirror_filter_id = aws_ec2_traffic_mirror_filter.zeek_filter[0].id
+  traffic_mirror_target_id = aws_ec2_traffic_mirror_target.zeek_target[0].id
+  network_interface_id     = var.windows_client_instance[0].primary_network_interface_id
+  session_number           = 300
 }
