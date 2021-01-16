3354969a897d28945f90046b82feb2e5072447ce
divious1 <josehelps@gmail.com>
2021-01-16 10:14:19 -0500

    removed unecessary formula

--- scripts/macos_deploy.sh
+++ scripts/macos_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 brew update
-brew install python awscli python-pip
+brew install python awscli
 pip3 install virtualenv
 curl -s https://releases.hashicorp.com/terraform/0.14.4/terraform_0.14.4_linux_amd64.zip -o terraform.zip
-unzip -o terraform.zip
+unzip terraform.zip
 sudo mv terraform /usr/local/bin/
 git clone https://github.com/splunk/attack_range && cd attack_range
 cd terraform/aws
