bd7090249b3ea15938f215728784c33267a60780
divious1 <josehelps@gmail.com>
2021-01-16 12:21:53 -0500

    adding git

--- scripts/macos_deploy.sh
+++ scripts/macos_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew update
-brew install python awscli
+brew install python awscli git
 pip3 install virtualenv
 curl -s https://releases.hashicorp.com/terraform/0.14.4/terraform_0.14.4_linux_amd64.zip -o terraform.zip
 unzip terraform.zip
