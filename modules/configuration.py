9d154fa61cc36a1221dcef14aeab03f379c014ae
divious1 <josehelps@gmail.com>
2021-01-21 11:45:20 -0500

    added keygen for azure

--- modules/configuration.py
+++ modules/configuration.py
@@ -4,18 +4,20 @@
 Helps configure your attack_range before using.
 '''
 
-import sys
+from Crypto.PublicKey import RSA
 from pathlib import Path
+from PyInquirer import prompt, Separator
+from botocore.config import Config
+import sys
 import argparse
 import urllib.request
-from PyInquirer import prompt, Separator
 import configparser
 import random
 import string
 import boto3
-from botocore.config import Config
 import getpass
 import time
+
 import os
 
 CONFIG_TEMPLATE = 'attack_range.conf.template'
@@ -40,8 +42,8 @@ def get_random_password():
     password = ''.join(password_list)
     return password
 
-def create_key_pair(client):
-    # create new ssh key new_key_pair
+def create_key_pair_aws(client):
+    # create new ssh key
     epoch_time = str(int(time.time()))
     ssh_key_name = getpass.getuser() + "-" + epoch_time[-5:] + ".key"
     # create ssh keys
@@ -49,8 +51,21 @@ def create_key_pair(client):
     with open(ssh_key_name, "w") as ssh_key:
         ssh_key.write(response['KeyMaterial'])
     os.chmod(ssh_key_name, 0o600)
-
     return ssh_key_name
+
+def create_key_pair_azure():
+    # create new ssh key
+    epoch_time = str(int(time.time()))
+    key = RSA.generate(2048)
+    priv_key_name = getpass.getuser() + "-" + epoch_time[-5:] + ".key"
+    pub_key_name = getpass.getuser() + "-" + epoch_time[-5:] + ".pub"
+    with open(priv_key_name, 'wb') as content_file:
+        os.chmod(priv_key_name, 0o600)
+        content_file.write(key.exportKey('PEM'))
+    pubkey = key.publickey()
+    with open(pub_key_name, 'wb') as content_file:
+        content_file.write(pubkey.exportKey('OpenSSH'))
+    return priv_key_name, pub_key_name
 
 def check_for_generated_keys(answers):
     keys = []
@@ -62,11 +77,33 @@ def check_for_generated_keys(answers):
     return False
 
 def get_generated_keys():
-    keys = []
+    priv_keys = []
+    pub_keys = []
     for file in os.listdir("."):
         if file.endswith(".key"):
-            keys.append(Path(file).resolve())
-    return keys
+            priv_keys.append(Path(file).resolve())
+        if file.endswith(".pub"):
+            pub_keys.append(Path(file).resolve())
+    if len(priv_keys) > 0:
+        priv_key = priv_keys[0]
+    else:
+        priv_key = ''
+
+    if len(pub_keys) > 0:
+        pub_key = pub_keys[0]
+    else:
+        pub_key = ''
+
+    return priv_key, pub_key
+
+def check_reuse_keys(answers):
+    if 'reuse_keys' in answers:
+        if answers['reuse_keys']:
+            return False
+        else:
+            return True
+    else:
+        return True
 
 def new(config):
     attack_range_config = Path(config)
@@ -158,6 +195,8 @@ starting configuration for AT-ST mech walker
         else:
             print("ERROR aws region not configured, please run `aws configure` to setup awscli")
             sys.exit(1)
+    else:
+        aws_configured_region = ''
     configuration._sections['global']['cloud_provider'] = answers['cloud_provider']
     configuration._sections['global']['attack_range_password'] = answers['attack_range_password']
     if 'azure_subscription_id' in answers:
@@ -170,16 +209,12 @@ starting configuration for AT-ST mech walker
     external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
     # get the latest key generated
-    keys = get_generated_keys()
-    if len(keys) > 0:
-        latest_key = keys[0]
-    else:
-        latest_key = ''
+    priv_key, pub_key  = get_generated_keys()
 
     questions = [
         {   # reuse key pair?
             'type': 'confirm',
-            'message': 'detected existing key in {0}, would you like to use it'.format(latest_key),
+            'message': 'detected existing key in {0}, would you like to use it'.format(priv_key),
             'name': 'reuse_keys',
             'default': True,
             'when': check_for_generated_keys,
@@ -189,7 +224,7 @@ starting configuration for AT-ST mech walker
             'message': 'generate a new ssh key pair for this range',
             'name': 'new_key_pair',
             'default': True,
-            'when': lambda keys: len(keys) == 0,
+            'when': check_reuse_keys,
         },
     ]
 
@@ -197,19 +232,34 @@ starting configuration for AT-ST mech walker
     answers = prompt(questions)
     if 'reuse_keys' in answers:
         if answers['reuse_keys']:
-            key_name = os.path.basename(os.path.normpath(latest_key))
-            configuration._sections['range_settings']['key_name'] = str(key_name)[:-4]
-            configuration._sections['range_settings']['private_key_path'] = str(latest_key)
-            print("> included ssh key: {}".format(latest_key))
+            priv_key_name = os.path.basename(os.path.normpath(priv_key))
+            configuration._sections['range_settings']['key_name'] = str(priv_key_name)[:-4]
+            configuration._sections['range_settings']['private_key_path'] = str(priv_key)
+            configuration._sections['range_settings']['public_key_path'] = str(pub_key)
+            print("> included ssh private key: {}".format(priv_key))
 
     if 'new_key_pair' in answers:
         if answers['new_key_pair']:
-            # create new ssh key new_key_pair
-            new_key_name = create_key_pair(aws_session.client('ec2', region_name=aws_configured_region))
-            new_key_path = Path(new_key_name).resolve()
-            configuration._sections['range_settings']['key_name'] = new_key_name[:-4]
-            configuration._sections['range_settings']['private_key_path'] = str(new_key_path)
-            print("> new aws ssh created: {}".format(new_key_path))
+            # create new ssh key for aws
+            if configuration._sections['global']['cloud_provider'] == "aws":
+                new_key_name = create_key_pair_aws(aws_session.client('ec2', region_name=aws_configured_region))
+                new_key_path = Path(new_key_name).resolve()
+                configuration._sections['range_settings']['key_name'] = new_key_name[:-4]
+                configuration._sections['range_settings']['private_key_path'] = str(new_key_path)
+                configuration._sections['range_settings']['public_key_path'] = str(pub_key)
+                print("> new aws ssh created: {}".format(new_key_path))
+            elif configuration._sections['global']['cloud_provider'] == "azure":
+                priv_key_name, pub_key_name = create_key_pair_azure()
+                priv_key_path = Path(priv_key_name).resolve()
+                pub_key_path = Path(pub_key_name).resolve()
+                configuration._sections['range_settings']['key_name'] = priv_key_name[:-4]
+                configuration._sections['range_settings']['private_key_path'] = str(priv_key_path)
+                configuration._sections['range_settings']['public_key_path'] = str(pub_key_path)
+                print("> new azure ssh pair created:\nprivate key: {0}\npublic key:{1}".format(priv_key_path, pub_key_path))
+            else:
+                print("ERROR, we do not support generating a key pair for the selected provider: {}".format(configuration._sections['global']['cloud_provider']))
+
+
 
     questions = [
         {
@@ -234,7 +284,7 @@ starting configuration for AT-ST mech walker
             'message': 'enter public key path for machine access',
             'name': 'public_key_path',
             'default': "~/.ssh/id_rsa.pub",
-            'when': lambda answers: configuration._sections['global']['cloud_provider'] == 'azure',
+            'when': lambda answers: configuration._sections['global']['cloud_provider'] == 'azure' and configuration._sections['range_settings']['public_key_path'] == '',
         },
         {
             # get region
@@ -265,15 +315,13 @@ starting configuration for AT-ST mech walker
     if 'key_name' in answers:
         configuration._sections['range_settings']['key_name'] = answers['key_name']
     else:
-        print("> using aws ssh key name: {}".format(configuration._sections['range_settings']['key_name']))
+        print("> using ssh key name: {}".format(configuration._sections['range_settings']['key_name']))
     if 'private_key_path' in answers:
         configuration._sections['range_settings']['private_key_path'] = answers['private_key_path']
     else:
-        print("> using aws ssh private key: {}".format(configuration._sections['range_settings']['private_key_path']))
+        print("> using ssh private key: {}".format(configuration._sections['range_settings']['private_key_path']))
     if 'public_key_path' in answers:
         configuration._sections['range_settings']['public_key_path'] = answers['public_key_path']
-    else:
-        configuration._sections['range_settings']['public_key_path'] = '~/.ssh/id_rsa.pub'
     if 'region' in answers:
         configuration._sections['range_settings']['region'] = answers['region']
     else:
@@ -350,6 +398,6 @@ starting configuration for AT-ST mech walker
     # write config file
     with open(attack_range_config, 'w') as configfile:
         configuration.write(configfile)
-    print("> configuration file was written to: {0}, run `python attack_range.py build` to create a new attack_range".format(Path(attack_range_config).resolve()))
+    print("> configuration file was written to: {0}, run `python attack_range.py build` to create a new attack_range\nyou can also edit this file to configure advance parameters".format(Path(attack_range_config).resolve()))
     print("> setup has finished successfully ... exiting")
     sys.exit(0)
