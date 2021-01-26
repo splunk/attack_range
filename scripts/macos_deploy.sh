db2179d2eb7dba31c1148e8558e67e9a35331e2f
divious1 <josehelps@gmail.com>
2021-01-26 12:10:39 -0500

    fixing bugs reported in the config tool

--- scripts/macos_deploy.sh
+++ scripts/macos_deploy.sh
@@ -9,6 +9,9 @@ git clone https://github.com/splunk/attack_range && cd attack_range
 cd terraform/aws
 terraform init
 cd ../..
+cd terraform/azure
+terraform init
+cd ../..
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
