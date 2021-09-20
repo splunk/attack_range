9b5b508565f1d23227259291f0ec9a8111c50782
P4T12ICK <pbareiss@splunk.com>
2021-09-20 08:30:38 +0200

    dev sec ops pacckage

--- scripts/dev_sec_ops_poc.sh
+++ scripts/dev_sec_ops_poc.sh
@@ -2,6 +2,32 @@
 
 rm -rf attack_data/*
 
+# GitHub Pull Request from Unknown User
+mkdir attack_data/github3
+wget -O attack_data/github3/github_pull_request.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1195.001/github_pull_request/github_pull_request.json
+python attack_range.py replay -dn github3 --source current_timestamp --sourcetype aws:firehose:json --index test --file_name github_pull_request.json
+sleep 10
+python attack_range.py search --search "ESCU - GitHub Pull Request from Unknown User - Rule" --earliest 2h
+
+sleep 60
+
+# Circle CI Disable Security Job
+mkdir attack_data/ci1
+wget -O attack_data/ci1/circle_ci_disable_security_job.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1554/circle_ci_disable_security_job/circle_ci_disable_security_job.json
+python attack_range.py replay -dn ci1 --source circleci --sourcetype circleci --index test --file_name circle_ci_disable_security_job.json
+sleep 10
+python attack_range.py search --search "ESCU - Circle CI Disable Security Job - Rule" --earliest 2h
+
+# Circle CI Disable Security Step
+mkdir attack_data/ci2
+wget -O attack_data/ci2/circle_ci_disable_security_step.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1554/circle_ci_disable_security_step/circle_ci_disable_security_step.json
+python attack_range.py replay -dn ci2 --source circleci --sourcetype circleci --index test --file_name circle_ci_disable_security_step.json
+sleep 10
+python attack_range.py search --search "ESCU - Circle CI Disable Security Step - Rule" --earliest 2h
+
+sleep 60
+
+
 # AWS ECR Container Scanning Findings High
 mkdir attack_data/aws_ecr_1
 wget -O attack_data/aws_ecr_1/aws_ecr_scanning_findings_events.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1204.003/aws_ecr_image_scanning/aws_ecr_scanning_findings_events.json
@@ -19,19 +45,12 @@ python attack_range.py replay -dn aws_ecr_2 --source current_timestamp --sourcet
 sleep 10
 python attack_range.py search --search "ESCU - AWS ECR Container Upload Unknown User - Rule" --earliest 2h
 
-# Circle CI Disable Security Job
-mkdir attack_data/ci1
-wget -O attack_data/ci1/circle_ci_disable_security_job.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1554/circle_ci_disable_security_job/circle_ci_disable_security_job.json
-python attack_range.py replay -dn ci1 --source circleci --sourcetype circleci --index test --file_name circle_ci_disable_security_job.json
+# GitHub Dependabot Alert
+mkdir attack_data/github2
+wget -O attack_data/github2/github_security_advisor_alert.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1195.001/github_security_advisor_alert/github_security_advisor_alert.json
+python attack_range.py replay -dn github2 --source current_timestamp --sourcetype aws:firehose:json --index test --file_name github_security_advisor_alert.json
 sleep 10
-python attack_range.py search --search "ESCU - Circle CI Disable Security Job - Rule" --earliest 2h
-
-# Circle CI Disable Security Step
-mkdir attack_data/ci2
-wget -O attack_data/ci2/circle_ci_disable_security_step.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1554/circle_ci_disable_security_step/circle_ci_disable_security_step.json
-python attack_range.py replay -dn ci2 --source circleci --sourcetype circleci --index test --file_name circle_ci_disable_security_step.json
-sleep 10
-python attack_range.py search --search "ESCU - Circle CI Disable Security Step - Rule" --earliest 2h
+python attack_range.py search --search "ESCU - GitHub Dependabot Alert - Rule" --earliest 2h
 
 
 # Github Commit Changes In Master
@@ -41,19 +60,6 @@ python attack_range.py replay -dn github1 --source current_timestamp --sourcetyp
 sleep 10
 python attack_range.py search --search "ESCU - Github Commit Changes In Master - Rule" --earliest 2h
 
-# GitHub Dependabot Alert
-mkdir attack_data/github2
-wget -O attack_data/github2/github_security_advisor_alert.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1195.001/github_security_advisor_alert/github_security_advisor_alert.json
-python attack_range.py replay -dn github2 --source current_timestamp --sourcetype aws:firehose:json --index test --file_name github_security_advisor_alert.json
-sleep 10
-python attack_range.py search --search "ESCU - GitHub Dependabot Alert - Rule" --earliest 2h
-
-# GitHub Pull Request from Unknown User
-mkdir attack_data/github3
-wget -O attack_data/github3/github_pull_request.json https://media.githubusercontent.com/media/splunk/attack_data/master/datasets/attack_techniques/T1195.001/github_pull_request/github_pull_request.json
-python attack_range.py replay -dn github3 --source current_timestamp --sourcetype aws:firehose:json --index test --file_name github_pull_request.json
-sleep 10
-python attack_range.py search --search "ESCU - GitHub Pull Request from Unknown User - Rule" --earliest 2h
 
 # Gsuite Drive Share In External Email
 mkdir attack_data/gsuite1
