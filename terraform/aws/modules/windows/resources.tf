a94512219b4173f84263a579f8548ddc550b661c
P4T12ICK <pbareiss@splunk.com>
2022-06-08 15:29:24 +0200

    improve CI/CD

--- terraform/aws/modules/windows/resources.tf
+++ terraform/aws/modules/windows/resources.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "windows_server" {
 $admin = [adsi]("WinNT://./Administrator, user")
 $admin.PSBase.Invoke("SetPassword", "${var.general.attack_range_password}")
 winrm quickconfig -q
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
