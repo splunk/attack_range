c961f9a7f89069c3c195b76a59a3521eeecdd7a8
P4T12ICK <pbareiss@splunk.com>
2022-07-25 14:49:20 +0200

    improve cicd

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
