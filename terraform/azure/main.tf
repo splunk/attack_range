e7505396183b4e1692b8d149c1cbe3a2dd67ef70
P4T12ICK <pbareiss@splunk.com>
2022-07-28 08:01:16 +0200

    bug fix azure

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
+
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
