a196997831765104323b9a422c7ff8619e9bb218
P4T12ICK <patrickbareiss1989@gmail.com>
2023-06-19 10:10:11 +0200

    Improve Attack Simulation

--- terraform/aws/modules/windows/resources.tf
+++ terraform/aws/modules/windows/resources.tf
@@ -77,7 +77,7 @@ EOF
 
   provisioner "local-exec" {
     working_dir = "../../packer/ansible"
-    command = "ansible-playbook -i '${self.public_ip},' windows.yml --extra-vars 'ansible_user=Administrator ansible_password=${var.general.attack_range_password} ansible_winrm_operation_timeout_sec=120 ansible_winrm_read_timeout_sec=150 ansible_port=5985 attack_range_password=${var.general.attack_range_password} ${join(" ", [for key, value in var.general : "${key}=\"${value}\""])} ${join(" ", [for key, value in var.splunk_server : "${key}=\"${value}\""])}'"
+    command = "ansible-playbook -i '${self.public_ip},' windows.yml --extra-vars 'ansible_user=Administrator ansible_password=${var.general.attack_range_password} ansible_winrm_operation_timeout_sec=120 ansible_winrm_read_timeout_sec=150 ansible_port=5985 attack_range_password=${var.general.attack_range_password} ${join(" ", [for key, value in var.general : "${key}=\"${value}\""])} ${join(" ", [for key, value in var.splunk_server : "${key}=\"${value}\""])} ${join(" ", [for key, value in var.simulation : "${key}=\"${value}\""])}'"
   }
 
   provisioner "local-exec" {
