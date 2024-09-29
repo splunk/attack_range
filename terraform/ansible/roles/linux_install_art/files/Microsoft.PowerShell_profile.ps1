cfb1a553e14830ddb24949119bbe36e6bf24aeda
James Young <39607528+splunk-james@users.noreply.github.com>
2024-09-29 14:01:40 +1000

    Create Microsoft.PowerShell_profile.ps1

---
+++ terraform/ansible/roles/linux_install_art/files/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+Import-Module "/root/AtomicRedTeam/invoke-atomicredteam/Invoke-AtomicRedTeam.psd1" -Force
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="/root/AtomicRedTeam/atomics"}
